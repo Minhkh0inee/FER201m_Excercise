cf1f88effb13da5f1efb939c6b8536227033ae01
MinhKhoi <mkhoi1776@gmail.com>
2023-06-07 22:26:57 +0700

    Functional Component

---
+++ Ex8/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Player from './components/Player';
+function App() {
+  return (
+    <div className="App">
+      <Navigation></Navigation>
+      <Player></Player>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ Ex8/my-app/src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+export default function Navigation() {
+  return (
+    <div>
+      <nav>
+        <ul className="nav">
+          <li className="nav-ele active">
+            <a href="#home">
+              <span class="text-dec-none">Home</span>
+            </a>
+          </li>
+          <li className="nav-ele">
+            <a href="#news">News</a>
+          </li>
+          <li className="nav-ele">
+            <a href="#about">About</a>
+          </li>
+          <li className="nav-ele">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+
