6ed34327afb5934cd145aac2f191b21c826aa35e
MinhKhoi <mkhoi1776@gmail.com>
2023-06-07 22:05:13 +0700

    React Component 2

---
+++ Ex7/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navigation from './components/Navigation';
+import Player from './components/Player';
+function App() {
+  return (
+    <div className="App">
+      <Navigation></Navigation>
+      <Player></Player>
+    </div>
+  );
+}
+
+export default App;
